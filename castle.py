3dd1c35f441bf64070198c22e7faacecba912155
Alex <turneral1@grinnell.edu>
2017-10-06 09:13:49 -0700

    Split remove_block into more manageable halves. Added additional commenting to remove_block and removed superfluous checks.

--- castle.py
+++ castle.py
@@ -61,7 +61,7 @@ class Castle:
             space_index += 1
             self.skip_space = True
 
-        if modify_right:  # TODO inserts take O(n)
+        if modify_right:  # TODO inserts take O(n) in worst case; find better DS
             self.spaces[self.current_row].insert(space_index,
                                                  Block(right_side + 1, space.index + space.width - right_side - 1))
 
@@ -74,27 +74,9 @@ class Castle:
         :param space_index: the space in question is self.spaces[self.current_row][space_index].
         """
         left_side, right_side = block.index - 1, block.index + block.width
-        left_in_bounds, right_in_bounds = left_side > 0, right_side < self.width  # immediate neighbors in bounds?
-        left_space_free, right_space_free = False, False  # is there a zero two spaces adjacent?
-        block_to_left, block_to_right = False, False  # is there a block two spaces adjacent?
-        left_overhang, right_overhang = False, False  # would building to left/right create an overhang?
-
-        if left_in_bounds:  # TODO comment
-            left_overhang = not self.block_grid[self.current_row - 1][left_side]  # true if nothing is to the left
-            if left_side > 0:
-                block_to_left = self.block_grid[self.current_row][left_side - 1]
-                left_space_free = not left_overhang and self.block_grid[self.current_row - 1][left_side - 1] and \
-                                  not block_to_left and not self.unavailable_column[left_side - 1]
-
-        if right_in_bounds:
-            right_overhang = not self.block_grid[self.current_row - 1][right_side]
-            if right_side < self.width - 1:
-                block_to_right = self.block_grid[self.current_row][right_side + 1]
-                right_space_free = not right_overhang and self.block_grid[self.current_row - 1][right_side + 1] and \
-                                   not block_to_right and not self.unavailable_column[right_side + 1]
-
-        increment_left = left_overhang or not left_in_bounds or (left_side > 0 and block_to_left)
-        decrement_right = right_overhang or not right_in_bounds or (right_side < self.width - 1 and block_to_right)
+        left_in_bounds, right_in_bounds, left_space_free, right_space_free, \
+            block_to_left, block_to_right, left_overhang, right_overhang, \
+            increment_left, decrement_right = self.get_booleans(block)
 
         # Remove the block
         self.last_id -= 1
@@ -119,16 +101,49 @@ class Castle:
         # Increment width because if left bound == right bound == 0, it's a one-width block
         new_space = Block(left_side, right_side - left_side + 1)
 
-        if left_space_free:  # todo comment and rename spaces
-            space = self.spaces[self.current_row].pop(space_index)
-            new_space = Block(space.index, new_space.width + space.width)
+        if left_space_free:  # merge with space on the left
+            left_space = self.spaces[self.current_row].pop(space_index)
+            new_space = Block(left_space.index, new_space.width + left_space.width)
 
-        if right_space_free:  # if open block to right, must be a space
-            new_space.width += self.spaces[self.current_row][space_index].width
-            self.spaces[self.current_row].pop(space_index)
+        if right_space_free:  # merge with space on the right
+            right_space = self.spaces[self.current_row].pop(space_index)
+            new_space.width += right_space.width
 
         # Add the space
         self.spaces[self.current_row].insert(space_index, new_space)
+
+    def get_booleans(self, block):
+        """Return a tuple of booleans concerning the surroundings of the given block.
+
+        :return left_in_bounds, right_in_bounds: are the indices immediately neighboring block in-bounds?
+        :return left_space_free, right_space_free: is there a space two indices away from the given side of block?
+        :return block_to_left, block_to_right: is there a block two indices away from the given side of block?
+        :return left_overhang, right_overhang: would building one square to the side create an overhang?
+        :return increment_left, decrement_right: should we narrow our estimate of the free space that will be created by
+         removing block?
+        """
+        left_side, right_side = block.index - 1, block.index + block.width
+        left_in_bounds, right_in_bounds = left_side > 0, right_side < self.width
+        left_space_free, right_space_free, block_to_left, block_to_right, left_overhang, right_overhang = [False] * 6
+
+        if left_in_bounds:
+            left_overhang = not self.block_grid[self.current_row - 1][left_side]  # true if nothing is to the left
+            if left_side > 0:  # if two squares to the left is in-bounds
+                block_to_left = self.block_grid[self.current_row][left_side - 1]
+                left_space_free = not left_overhang and not block_to_left and not self.unavailable_column[left_side - 1]
+
+        if right_in_bounds:
+            right_overhang = not self.block_grid[self.current_row - 1][right_side]
+            if right_side < self.width - 1:  # if two squares to the right is in-bounds
+                block_to_right = self.block_grid[self.current_row][right_side + 1]
+                right_space_free = not right_overhang and not block_to_right and \
+                    not self.unavailable_column[right_side + 1]
+
+        increment_left = left_overhang or not left_in_bounds or block_to_left
+        decrement_right = right_overhang or not right_in_bounds or block_to_right
+
+        return left_in_bounds, right_in_bounds, left_space_free, right_space_free, block_to_left, block_to_right, \
+            left_overhang, right_overhang, increment_left, decrement_right
 
     def add_space_above(self, space):
         """Add a space to the next row, if there is one.
